390716005fadbdb5a24a59e74a12043cf8421aa3
Jacques Erasmus <jerasmus@gitlab.com>
2022-07-13 10:52:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ define(
   ],
 
   function(advice, component, compose, debug, logger, registry, utils) {
+    'use strict.';
+
+    return {
+      advice: advice,
+      component: component,
+      compose: compose,
+      debug: debug,
+      logger: logger,
+      registry: registry,
+      utils: utils
+    };
+  },
+
+  function(advice, component, compose, debug, logger, registry, utils) {
     'use strict';
 
     return {
