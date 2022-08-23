1d914aee2a1f5348ed49d6cc7d246c5f4c07db8a
Jacques Erasmus <jerasmus@gitlab.com>
2022-08-23 11:31:23 +0000

    Add new file

---
+++ syntax/python3/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+>>> x = input("something:")
+something:10
+
+>>> x
+'10'
+
+>>> x = input("something:")
+something:'10' #entered data treated as string with or without ''
+
+>>> x
+"'10'"
