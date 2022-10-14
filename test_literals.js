c925d782e51fd4978c0e8d1d6d64997bc1f31183
Jacques Erasmus <jerasmus@gitlab.com>
2022-10-14 10:39:01 +0000

    Add new file

---
+++ test_literals.js
@@ -0,0 +1,10 @@
+function test(a) {
+    let b = "";
+
+    for(let i=0; i < a; i++) {
+        b += `<div>
+        <span>${data[Math.floor(Math.random() * data.length)]}</span>
+        <id>3</id>
+        </div>`
+    }
+}
