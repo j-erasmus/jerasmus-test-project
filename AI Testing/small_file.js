67555d3354db3d84b0bb3e0450fed10e87490207
Jacques Erasmus <jerasmus@gitlab.com>
2023-04-26 17:49:57 +0000

    Upload New File

---
+++ AI Testing/small_file.js
@@ -0,0 +1,14 @@
+// Illustration of the creating and use of functions
+
+function exampleFunction(value1, value2) {
+    var result = value1 * value2;
+
+    return result;
+}
+
+var first = prompt("Enter a number: ")
+var second = prompt("Enter a second number: ")
+
+console.log("The product of " + first + " and " + second + " is " + exampleFunction(first, second))
+// or in a browser
+document.write("The product of " + first + " and " + second + " is " + exampleFunction(first, second))
