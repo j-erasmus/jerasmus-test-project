e91e01fc3cb7fbae68c16ec51635c90d9a5d0888
Jacques Erasmus <jerasmus@gitlab.com>
2023-05-31 08:15:36 +0000

    Update haskell_sample.hs

--- syntax/haskell/haskell_sample.hs
+++ syntax/haskell/haskell_sample.hs
@@ -1,7 +1,7 @@
 jsonStringContentP :: Parser String
 jsonStringContentP = wrapped1P dP $ tseqP dP (charP '\\') passChar
   where
-      dP = charP '"' -- obiect delimiter
+      dP = charP '"' -- this is a comment
 
 jsonString :: Parser JsonValue
 jsonString = JsonString <$> jsonStringContentP
